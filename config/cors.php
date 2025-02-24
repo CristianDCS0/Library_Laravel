6f4c509b13785955917e4660806e6ed316a6c6a8
Cristian Cobaxin <cdcs030600@gmail.com>
2025-02-24 15:46:20 -0600

    update cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://localhost:8000'],
+    'allowed_origins' => ['http://localhost:5173', 'https://library-vue-ashy.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
