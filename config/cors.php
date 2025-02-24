669d9086dc7ab75d0710b1c75f6b728961e8a330
Cristian Cobaxin <cdcs030600@gmail.com>
2025-02-24 17:34:13 -0600

    mod cors for vercel 3

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://library-vue-ashy.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
